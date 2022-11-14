59753a51db4da522f696f0241f2bc9641000fab0
awaik <awaiking44@gmail.com>
2022-11-14 17:39:46 +0200

    removed methods from calculator

--- lib/src/calculator.dart
+++ lib/src/calculator.dart
@@ -2,26 +2,12 @@ class Calculator {
   int result = 0;
 
 
-  double universalInput(var input) {
-    if (input is String) {
-      parseListAndAddFoundedNumbers
-    }
-    if (input is File) {
-      parseVoiceInput
-    }
-
-  }
-
   double? parseListAndAddFoundedNumbers(String input) {
     if (input.contains('+')) {
       final List<String> inputs = input.split('+');
       return addition(int.parse(inputs[0]), int.parse(inputs[1])).toDouble();
     }
     return null;
-  }
-
-  double parseVoiceInput(File voiceFile) {
-
   }
 
 
